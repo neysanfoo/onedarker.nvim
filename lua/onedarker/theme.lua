b92126c23185d0dcdc4b0f7e13278d0385164bc3
“NeysanNTU” <neys0001@e.ntu.edu.sg>
2022-09-27 00:54:17 +0800

    comment gray

--- lua/onedarker/theme.lua
+++ lua/onedarker/theme.lua
@@ -90,7 +90,7 @@ theme.set_highlights = function()
   hl(0, "TabLineFill", { fg = c.line, bg = c.line })
 
   -- Code
-  hl(0, "Comment", { fg = c.blue, bg = "NONE", italic = italic_comments })
+  hl(0, "Comment", { fg = c.comment_gray, bg = "NONE", italic = italic_comments })
   hl(0, "Variable", { fg = c.fg, bg = "NONE" })
   hl(0, "String", { fg = c.green, bg = "NONE" })
   hl(0, "Character", { fg = c.green, bg = "NONE" })
